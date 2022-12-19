294fdf42a895bef2a0859371e583478a1b953314
freeayoub <gayoub2009@gmail.com>
2022-12-19 10:18:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1969.4822279041368!2d10.17734174306939!3d36.79894741356051!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x644f4bb3e053f88e!2sGOMYCODE%20Tunis%20Centre%20Ville!5e0!3m2!1sfr!2sus!4v1671440530488!5m2!1sfr!2sus" width="600" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="mape"></iframe>
+    </div>
+  );
+}
+
+export default App;
